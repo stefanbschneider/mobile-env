066d27bf999a0fd55a0720733f16b8a9c84553d8
Stefan Schneider <28340802+stefanbschneider@users.noreply.github.com>
2026-01-16 11:43:20 +0100

    Try to update ray, but notebook fails. Impossible to debug
    Doesn't stop at breakpoints or when raising exception -- can't figure out where the issue is

--- mobile_env/wrappers/multi_agent.py
+++ mobile_env/wrappers/multi_agent.py
@@ -20,18 +20,26 @@ class RLlibMAWrapper(MultiAgentEnv):
             assert isinstance(env.unwrapped, MComCore), "The unwrapped env should be a mobile-env."
             self.env = env.unwrapped
 
+        # Set possible agents to all users (required by RLlib).
+        self.possible_agents = list(self.env.users.keys())
+        # Active agents/users are set during reset() and step().
+        self.agents = None
+
         # set max. number of steps for RLlib trainer
         self.max_episode_steps = self.env.EP_MAX_TIME
 
         # override action and observation space defined for wrapped environment
         # RLlib expects the action and observation space
         # to be defined per actor, i.e, per UE
-        self.action_space = gymnasium.spaces.Discrete(self.env.NUM_STATIONS + 1)
+        self.action_spaces = {
+            ue_id: gymnasium.spaces.Discrete(self.env.NUM_STATIONS + 1)
+            for ue_id in self.possible_agents
+        }
         size = self.env.handler.ue_obs_size(self.env)
-        self.observation_space = gymnasium.spaces.Box(
-            low=-1, high=1, shape=(size,), dtype=np.float32
-        )
-
+        self.observation_spaces = {
+            ue_id: gymnasium.spaces.Box(low=-1, high=1, shape=(size,), dtype=np.float32)
+            for ue_id in self.possible_agents
+        }
         # track UE IDs of last observation's dictionary, i.e.,
         # what UEs were active in the previous step
         self.prev_step_ues: Optional[set[int]] = None
@@ -39,6 +47,7 @@ class RLlibMAWrapper(MultiAgentEnv):
     def reset(self, *, seed=None, options=None) -> MultiAgentDict:
         obs, info = self.env.reset(seed=seed, options=options)
         self.prev_step_ues = set(obs.keys())
+        self.agents = [ue.ue_id for ue in self.env.active]
         return obs, info
 
     def step(
@@ -46,19 +55,21 @@ class RLlibMAWrapper(MultiAgentEnv):
     ) -> Tuple[MultiAgentDict, MultiAgentDict, MultiAgentDict, MultiAgentDict, MultiAgentDict]:
         obs, rews, terminated, truncated, infos = self.env.step(action_dict)
 
+        self.agents = [ue.ue_id for ue in self.env.active]
+
         # UEs that are not active after `step()` are done (here: truncated)
         # NOTE: `truncateds` keys are keys of previous observation dictionary
+        # TODO: Clean up what processing happens in the wrapper vs. in the handler.
         assert self.prev_step_ues is not None
         inactive_ues = self.prev_step_ues - set([ue.ue_id for ue in self.env.active])
         truncateds: MultiAgentDict = {
-            ue_id: True if ue_id in inactive_ues else False
-            for ue_id in self.prev_step_ues
+            ue_id: True if ue_id in inactive_ues else False for ue_id in self.prev_step_ues
         }
         truncateds["__all__"] = truncated
         # Terminated is always False since there is no particular terminal end state.
-        assert (
-            not terminated
-        ), "There is no natural episode termination. terminated should be False."
+        assert not terminated, (
+            "There is no natural episode termination. terminated should be False."
+        )
         terminateds: MultiAgentDict = {ue_id: False for ue_id in self.prev_step_ues}
         terminateds["__all__"] = False
 
